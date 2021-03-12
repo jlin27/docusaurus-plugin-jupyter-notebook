aba8ef130441256c443a0db4cd9a2e33cbe37614
Jessica Lin <jplin@fb.com>
2021-03-12 15:54:22 -0800

    Update naming pattern for TestJupyterComponent for it to work

---
+++ my-website/src/pages/TestJupyterComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TestJupyterComponent extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>This is a placeholder for a jupyter notebook component</h1>
+      </div>
+    );
+  }
+}
+
+export default TestJupyterComponent;
