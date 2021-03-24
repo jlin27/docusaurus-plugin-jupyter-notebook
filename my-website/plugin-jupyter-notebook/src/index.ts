16b52f1a61b9449c43d7aff015ce33d8cb27c41d
Jessica Lin <jplin@fb.com>
2021-03-24 15:04:48 -0700

    Cleanup demo plugin

--- my-website/plugin-jupyter-notebook/src/index.ts
+++ my-website/plugin-jupyter-notebook/src/index.ts
@@ -1,9 +1,8 @@
-// Currently this is just a dummy plug-in. The following to be added in
-// .ipynb -> .html via nbconvert
-// .html -> .jsx via https://github.com/reactjs/react-magic/blob/master/README-htmltojsx.md
-// Add the converted jsx notebook as a mdx component in a doc page
-
-
-export function hello_world(name:string){
-    console.log(`Hello ${name}`);
+module.exports = function() {
+    return {
+        name: 'plugin-jupyter-notebook',
+        async loadContent() {
+            console.log("This is a test jupyter plugin");
+        }
+    }
 }
