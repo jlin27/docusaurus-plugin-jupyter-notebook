a6bcc7c4d73d7b1b84ec7cfbb2677285ac5fae7c
Jessica Lin <jplin@fb.com>
2021-03-25 13:10:01 -0700

    Add plans in index.ts

--- my-website/plugin-jupyter-notebook/src/index.ts
+++ my-website/plugin-jupyter-notebook/src/index.ts
@@ -1,3 +1,19 @@
+/*
+Plans for acutal plug-in:
+- In index.ts
+    - Get pluginOptions
+        - runJupyter //Generate Jupyter notebook docs, true or false
+        - docusaurusJupyterSubDir //Directory where the generated mdx pages go
+        - jupyterDir //Where do the original .ipynb files live
+    - Calls integrateJupyterDocs
+- In integrateJupyterDocs.js
+    - Load context and pluginOptions
+- Calls
+    - generateJupyterHtml
+    - generateJupyterHtmlToJsx
+    - moveJupyterJsxToMdx
+*/
+
 module.exports = function() {
     return {
         name: 'plugin-jupyter-notebook',
